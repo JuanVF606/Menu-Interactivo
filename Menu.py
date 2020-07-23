ff72f4ba15adb534993bd3872aa180cbb26a8c7d
Juan Venegas <mimo.bien@gmail.com>
2020-07-23 19:36:43 -0400

    he agregado la opcion numero 2

--- Menu.py
+++ Menu.py
@@ -1,17 +1,19 @@
-import Buscaminas 
-
+import Buscaminas, TresEnRayas
 
 def CategoriaJuegos(opcion):
     if opcion == 1:
         Buscaminas.Menu()
+    if opcion ==2:
+        TresEnRayas.Menu()
 
 
 def Menu():
     print('Bienvenido Al Menu interactivo')
     print('------------------------------')
-    Categorias =int(input(' 1.Buscaminas 2.Tres en raya'))
+    Categorias =int(input(' 1.Buscaminas 2.Tres en raya: '))
     CategoriaJuegos(Categorias)
+
 
 #BLOQUE PRINCIPAL
   
-Menu() 
+Menu()
