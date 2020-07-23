5365ef72ade8e9bb6cde0c9e5aac54d0f3ea7f54
Juan Venegas <mimo.bien@gmail.com>
2020-07-23 18:12:27 -0400

    Se a modificado las notas y se agrego una opcion en el buscaminas

--- Buscaminas.py
+++ Buscaminas.py
@@ -1,13 +1,108 @@
 #Mensajes:
 M1 = 'Bienvenido al Juego buscaminas'
-M2 = 'Por favor dijite una opcion:'
+
 
 #                           DEFINICIONES
-#Configuracion del buscaminas
-from random import randint
+#Configuracion del buscaminas PREDETERMINADO:
+def BuscaminasPredet():
+    n = 6
+    T = []
+    i = 1
+    while (i<=n):
+        F = ['.'] * n
+        T.append(F)
+        i = i+1
+    strPos = input('Ingresa  3 posiciones aleatorias: ')
+    strPos = strPos.upper()
+    numMinas = 0
+    i=0
+    while i<len(strPos):
+        if T[ord(strPos[i])-65][int(strPos[i+1])-1] != '*':
+            T[ord(strPos[i])-65][int(strPos[i+1])-1] = '*'
+            numMinas += 1
+        i=i+2
+    celdasSinMinas = n*n-numMinas
+    print('', end='  ')
+    j=0
+    while j<n:
+        print(j+1, end=' ')
+        j=j+1
+    print('', end='\n')
+    i=0
+    while i<n:
+        j=0
+        print(chr(i+65), end=' ') #Letra al principio de cada fila.
+        while j<n:
+            if T[i][j] == '*':
+                print('.', end=' ')
+            else:
+                print(T[i][j], end=' ')
+            j=j+1
+        print('', end='\n')
+        i=i+1
+    #INICIO DEL JUEGO:
+    ganaste = False
+    perdiste = False
+    while perdiste == False and ganaste == False:
+        strPos = input('Ingresa la casilla del tablero que quieres abrir: ')
+        strPos = strPos.upper()
+        x = ord(strPos[0])-65
+        y = int(strPos[1])-1
+        if 0 <= x < n and 0 <= y < n:
+            contMinas = 0
+            if T[x][y] == '*':
+                perdiste = True
+            elif T[x][y] == '.':
+                if x>0 and y>0 and T[x-1][y-1] == '*': #ARRIBA-IZQUIERDA
+                    contMinas += 1
+                if x>0 and T[x-1][y] == '*': #ARRIBA
+                    contMinas += 1
+                if x>0 and y<n-1 and T[x-1][y+1] == '*': #ARRIBA-DERECHA
+                    contMinas += 1
+                if y>0 and T[x][y-1] == '*': #IZQUIERDA
+                    contMinas += 1
+                if y<n-1 and T[x][y+1] == '*': #DERECHA
+                    contMinas += 1
+                if x<n-1 and y>0 and T[x+1][y-1] == '*': #ABAJO-IZQUIERDA
+                    contMinas += 1
+                if x<n-1 and T[x+1][y] == '*': #ABAJO
+                    contMinas += 1
+                if x<n-1 and y<n-1 and T[x+1][y+1] == '*': #ABAJO-DERECHA
+                    contMinas += 1
+                T[x][y] = str(contMinas)
+                celdasSinMinas -= 1        
+        if celdasSinMinas == 0:
+            ganaste = True
+        print('', end='  ')
+        j=0
+        while j<n:
+            print(j+1, end=' ')
+            j=j+1
+        print('', end='\n')
+        i=0
+        while i<n:
+            j=0
+            print(chr(i+65), end=' ')   #Letra al principio de cada fila.
+            while j<n:
+                if perdiste == True or ganaste == True:
+                    print(T[i][j], end=' ')
+                else:
+                    if T[i][j] == '*':
+                        print('.', end=' ')
+                    else:
+                        print(T[i][j], end=' ')
+                j=j+1
+            print('', end='\n')
+            i=i+1
+        if ganaste == True:
+            print('GANASTE')
+        if perdiste == True:
+            print('PERDISTE')        
 
-#Configuracion Juego Buscaminas:
+#Configuracion Juego Buscaminas con manejo de archivos :
+from random import randint
 def MINAS_ALEATORIAS(Nivel_DIFICULTAD,dimension):
+
     if Nivel_DIFICULTAD == "F":
         Porcentaje_Minas = 10
     elif Nivel_DIFICULTAD == "M":
@@ -198,13 +293,13 @@ def Dificultades():
 
 #Opciones:
 def opciones():
-    _print = int(input('(1) Tablero Personalizado (2) Tablero Automatico (3) Salir: '))
+    _print = int(input('(1) Tablero Personalizado. (2) Tablero con manejos de Archivos. (3) Tablero con ingreso de minas por el Usuario. (4) Salir:  '))
     if _print == 1:
         print('Proximamente')
     if _print == 2:
         Dificultades()
-    if _print == 3 or _print != 3:
-        exit
+    if _print == 3:
+        BuscaminasPredet()
         
 #Menu Del juego:
 def Menu():
