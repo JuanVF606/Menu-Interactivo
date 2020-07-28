e113e90ced24bf19ff5de816c5bc770c5db24d91
Juan Venegas <mimo.bien@gmail.com>
2020-07-28 17:56:13 -0400

    Se le agregado el os.system(cls)

--- Menu.py
+++ Menu.py
@@ -6,14 +6,15 @@ def CategoriaJuegos(opcion):
     if opcion ==2:
         TresEnRayas.JUEGO()
 
-
+import os
 def Menu():
     print('Bienvenido Al Menu interactivo')
     print('------------------------------')
     Categorias =int(input(' 1.Buscaminas 2.Tres en raya: '))
+    os.system("cls")
     CategoriaJuegos(Categorias)
 
 
 #BLOQUE PRINCIPAL
 
-# Menu()
+Menu()
